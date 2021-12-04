b558d39725a7725adcbc2372f20446d08128bb5b
Qingfeng <1513267106@qq.com>
2021-12-04 15:19:48 +0800

    second commit

--- ES6/test1.js
+++ ES6/test1.js
@@ -2,31 +2,31 @@ let p1 = new Promise((resolve,reject) => {
     setTimeout(()=>{
         resolve('异步1')
     },1000)
-})
+});
 
 let p2 = new Promise((resolve,reject) => {
     setTimeout(()=>{
         resolve('异步2')
     },2000)
-})
+});
 
 let p3 = new Promise((resolve,reject) => {
     setTimeout(()=>{
         resolve('异步3')
     },3000)
-})
+});
 
-p1.then(value => console.log(value) )
+p1.then(value => console.log(value) );
 
 
 let test  = async() =>{
     let relute1 = await p1,
         relute2 = await p2,
-        relute3 = await p3
+        relute3 = await p3;
 
-    console.log(relute1 +' async')
-    console.log(relute2 +' async')
+    console.log(relute1 +' async');
+    console.log(relute2 +' async');
     console.log(relute3 +' async')
-}
+};
 
 test();
